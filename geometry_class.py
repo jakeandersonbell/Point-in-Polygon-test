3aafe3504c704b007e66a0ad88af1bf5066c18e1
unknown <jakeandersonbell@gmail.com>
2019-11-02 07:57:55 +0000

    Ray casting algorithm working

--- geometry_class.py
+++ geometry_class.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+import math
 
 
 class Geometry:  # The base class to hold name and name method
@@ -16,6 +18,9 @@ class Point(Geometry):  # Points class to have methods get_x, get_y
         super().__init__(name)
         self.__x = x
         self.__y = y
+        self.__in_bb = False
+        self.__on_boundary = False
+        self.__count = 0
 
     def get_x(self):
         return self.__x
@@ -23,19 +28,60 @@ class Point(Geometry):  # Points class to have methods get_x, get_y
     def get_y(self):
         return self.__y
 
+    def get_coords(self):
+        return [self.__x, self.__y]
 
-class Line(Geometry):  # Construct line given 2 points
+    def set_bb_rel(self, state):  # bounding box relationship
+        self.__in_bb = state
+
+    def get_bb_rel(self):
+        return self.__in_bb
+
+    def set_boundary_rel(self, state):  # boundary relationship
+        self.__on_boundary = state
+
+    def get_boundary_rel(self):
+        return self.__on_boundary
+
+
+class Line(Geometry):  # Construct line given a name and 2 points yx lists, then constructs point objects
 
     def __init__(self, name, p1, p2):
         super().__init__(name)
-        self.__p1 = p1
-        self.__p2 = p2
+        self.__p1 = Point(str(name) + ": 1", p1[0], p1[1])
+        self.__p2 = Point(str(name) + ": 2", p2[0], p2[1])
+        self.__count = 0
+
+    def get_p1(self):
+        return self.__p1
+
+    def get_p2(self):
+        return self.__p2
+
+    def is_parallel(self):  # Returns
+        if self.__p1.get_y() == self.__p2.get_y():
+            parallel_x = True
+        else:
+            parallel_x = False
+        if self.__p1.get_x() != math.inf:
+            if self.__p1.get_x() == self.__p2.get_x():
+                parallel_y = True
+            else:
+                parallel_y = False
+            return [parallel_x, parallel_y]
+
+    def increment(self):
+        self.__count += 1
+
+    def get_count(self):
+        return self.__count
 
 
 class Polygon(Geometry):
     # Initialize object with a list of point pairs as second argument
     # Currently has methods get_points and make points but will just use one of these for final program
     # Will use both for testing and go with the best option
+    # Convert all points to float on construction
     def __init__(self, name, points):
         super().__init__(name)
         self.__points = points
@@ -50,12 +96,18 @@ class Polygon(Geometry):
     def get_lines(self):  # Return a list of Line objects
         points = self.make_points()
         lines = []
-        point_a = points[0]
-        for point_b in points[1:]:
-            lines.append(Line(point_a.get_name() + "-" + point_b.get_name(), point_a, point_b))
-            point_a = point_b
-        lines.append(Line(point_a.get_name() + "-" + points[0].get_name(), point_a, points[0]))
+        pnt_a = points[0]
+        for pnt_b in points[1:]:
+            lines.append(Line(pnt_a.get_name() + "-" + pnt_b.get_name(), pnt_a.get_coords(), pnt_b.get_coords()))
+            pnt_a = pnt_b
+        lines.append(Line(pnt_a.get_name() + "-" + points[0].get_name(), pnt_a.get_coords(), points[0].get_coords()))
         return lines
+
+    def all_x(self):
+        return [i[0] for i in self.get_points()]
+
+    def all_y(self):
+        return [i[1] for i in self.get_points()]
 
 
 class Triangle(Polygon):
--- main_from_file.py
+++ main_from_file.py
@@ -2,6 +2,7 @@
 import geometry_class as geo
 import csv
 import matplotlib.pyplot as plt
+import math
 import time
 
 # Read a list of x, y coordinates from a csv and create a polygon object
@@ -9,47 +10,70 @@ import time
 #       Make list of points into a polygon object
 # Make these methods in geometry???
 with open('bb_test_square.csv', 'r') as file:
-    data = csv.reader(file)
-    next(data)  # This skips the csv header
+    data = [d.strip().split(',') for d in file.readlines()[1:]]  # This skips the csv header, creates a stripped list
     pl1 = geo.Polygon("Name", [list(map(float, row[1:3])) for row in data])  # Reads float(rows) into polygon object
+
 
 # Read a list of x, y coordinates from a csv to create a list of points
 #       make list of points into a list of point objects
 with open('bb_test_point.csv', 'r') as file:
     data = csv.reader(file)
     next(data)  # This skips the csv header
-    # Same as before except we are executing the make_points() Polygon method - not the mot intuitive???
+    # Same as before except we are executing the make_points() Polygon method - not the most intuitive???
     ps1 = geo.Polygon("Name", [list(map(float, row[1:3])) for row in data]).make_points()
 
-# Categorises point as being inside, outside or boundary
-#   Check minimum bounding rectangle
+
+# plt.fill(pl1.all_x(), pl1.all_y())
+# plt.plot([p.get_x() for p in ps1], [p.get_y() for p in ps1], 'r+')
+# plt.show()
+
+# Check if point is inside or on boundary of bounding box rectangle -
 for point in ps1:
-    if min(pl1.all_x()) < point.get_x() < max(pl1.all_x()) and min(pl1.all_y()) < point.get_y() < max(pl1.all_y()):
+    if min(pl1.all_x()) <= point.get_x() <= max(pl1.all_x()) and min(pl1.all_y()) <= point.get_y() <= max(pl1.all_y()):
         point.set_bb_rel(True)
     else:
         point.set_bb_rel(False)
 
-for p in ps1:
-    print(p.get_bb_rel())
-#       if min_x < point.get_x() < max_x and min_y < point.get_y() < max_y:
-#           point in bb
-#       else:
-#           point can't be inside polygon
-#   Ray casting - General rule
-#       init line class from point to math.inf along x
-#           for line in polygon:
-#               if ray intersects line:
-#                   count += 1
-# Look to move the following to its own .py
-#   Ray casting checks
-#       Point on line - line = [x1, y1], [x2, y2] - point = x3, y3
-#           if x1 == x2:
-#               line.parallel_x = True
-#               if x1 == x3:  # Point on line
-#           elif y1 == y2:
-#               parallel_y == True
-#               if x1 == y3:  # Point on line
-#           elif ((x3-x1)/(x2-x1))*(y2-y1)+y == y3: # Point on line
+# Check if points lie on parallel line
+for line in pl1.get_lines():  # Loop through lines
+    for point in (x for x in ps1 if not x.get_boundary_rel()):  # Loop through points that haven't been checked as True
+        if line.is_parallel()[0]:  # X parallel
+            if point.get_x() == line.get_p1().get_x():  # if point is on line
+                point.set_boundary_rel(True)
+            else:
+                point.set_boundary_rel(False)
+        elif line.is_parallel()[1]:  # Y parallel
+            if point.get_y() == line.get_p1().get_y():  # if point is on line
+                point.set_boundary_rel(True)
+            else:
+                point.set_boundary_rel(False)
+
+for point in ps1:
+    print(str(ps1.index(point)) + str(point.get_boundary_rel()))
+
+rays = [geo.Line("Ray "+str(ps1.index(p)+1), p.get_coords(), [math.inf, p.get_y()]) for p in ps1]
+
+for line in pl1.get_lines():  # [x3, y3], [x4, y4]
+    for ray in rays:  # [x1, y1], [x inf, y2]
+        y1, y3, y4 = ray.get_p1().get_y(), line.get_p1().get_y(), line.get_p2().get_y()  # y coords
+        x1, x3, x4 = ray.get_p1().get_x(), line.get_p1().get_x(), line.get_p1().get_x()  # x coords
+        if line.is_parallel()[0]:  # both X parallel
+            if line.get_p1().get_x() == ray.get_p1().get_x():  # lines intersect infinitely
+                pass
+            else:  # Don't intersect
+                pass
+        elif y3 < y1 < y4 or y3 > y1 > y4:  # point within line bounding box
+            x = x3 + (y1 * (x4 - x3))/((y4 - y3) + y3)  # line intersection equation
+            if x >= x1:  # ray intersects line
+                ray.increment()
+
+
+plt.fill(pl1.all_x(), pl1.all_y())
+plt.plot([p.get_x() for p in ps1], [p.get_y() for p in ps1], 'r+')
+for i, txt in enumerate(p.get_count() for p in rays):  # plot number of intersections on points
+    plt.annotate(txt, ([p.get_x() for p in ps1][i], [p.get_y() for p in ps1][i]))
+plt.show()
+
 #       Line crossing - line1 = [x1, y1], [xinf, y2] - line2 = [x3, y3], [x4, y4]
 #           if line1.parallel_x and line2.parallel_x:
 #               if y1 == y3:  # lines intersect inf
