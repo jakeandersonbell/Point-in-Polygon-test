772b7dd3db96b82e3c65ef6b25f02cebcaea4adc
unknown <jakeandersonbell@gmail.com>
2019-11-05 17:39:36 +0000

    Program working with inverted polygon

--- main_from_file.py
+++ main_from_file.py
@@ -1,6 +1,7 @@
 import geometry_class as geo
 import matplotlib.pyplot as plt
-import tests as test
+import test_old as test
+import math
 
 # Read a list of x, y coordinates from a csv and create a polygon object
 #   With open csv as file
@@ -15,7 +16,12 @@ with open('polygon.csv', 'r') as file:
 with open('input.csv', 'r') as file:
     data = [d.strip().split(',') for d in file.readlines()[1:]]  # This skips the csv header
     # Same as before except we are executing the make_points() Polygon method - not the most intuitive???
-    ps1 = geo.Polygon("Name", [list(map(float, row[1:3])) for row in data]).make_points()
+    ps1 = geo.Polygon("Name", [list(map(float, row[1:3])) for row in data])
+
+pl1.invert()
+ps1.invert()
+ps1 = ps1.make_points()
+
 
 test.in_bb(ps1, pl1)
 
@@ -23,11 +29,12 @@ test.on_line([p for p in ps1 if p.get_bb_rel()], pl1)
 
 test.ray_caster([p for p in ps1 if not p.get_state() == "Boundary"], pl1)
 
-plt.fill(pl1.all_x(), pl1.all_y())
-plt.plot([p.get_x() for p in ps1], [p.get_y() for p in ps1], 'r+')
-for i, txt in enumerate(p.get_state() for p in ps1):  # plot number of intersections on points
-    plt.annotate(txt, ([p.get_x() for p in ps1][i], [p.get_y() for p in ps1][i]))
-plt.show()
+
+# plt.fill(pl1.all_x(), pl1.all_y())
+# plt.plot([p.get_x() for p in ps1], [p.get_y() for p in ps1], 'r+')
+# for i, txt in enumerate(p.get_state() for p in ps1):  # plot number of intersections on points
+#     plt.annotate(txt, ([p.get_x() for p in ps1][i], [p.get_y() for p in ps1][i]))
+# plt.show()
 
 
 
