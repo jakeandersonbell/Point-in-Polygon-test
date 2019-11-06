7394aa0d3667dd6115fd6b221710b2296c18d9c9
unknown <jakeandersonbell@gmail.com>
2019-11-06 15:47:09 +0000

    Code is cleanly formatted with appropriate comments

--- geometry_class.py
+++ geometry_class.py
@@ -1,46 +1,68 @@
-import math
+
+"""This file holds all classes and methods for geometry objects."""
 
 
-def read_csv(file, geo_type, *name):  # Allows the creation of geo objects directly from a cvs read
+def read_csv(file, geo_type, *name):
+    """Creates geometry objects
+     directly from a cvs read.
+    """
     with open(file, 'r') as file:
         data = [d.strip().split(',') for d in
                 file.readlines()[1:]]  # This skips the csv header, creates a stripped list
-        shape = Geometry(name, [list(map(float, row[1:3])) for row in data])  # Reads float(rows) into polygon object
-    if geo_type.lower() == "points":
+        # Reads rows into polygon object, map function used to apply float function to each str item in the row list
+        shape = MultiGeometry(name, [list(map(float, row[1:3])) for row in data])
+    if geo_type.lower() == 'points':
         return shape.make_points()
-    elif geo_type.lower() == "polygon":
+    elif geo_type.lower() == 'polygon':
         return shape.make_poly()
 
 
-class Geometry:  # The base class to hold name and name method
-
-    # Define a class geometry with private name attribute
-    def __init__(self, name, points):
+class Geometry:
+    """Defines the base class to hold name,
+    all objects hereafter inherit name.
+    """
+    def __init__(self, name):
         self.__name = name
-        self.__points = points
 
     def get_name(self):
         return self.__name
 
+
+class MultiGeometry(Geometry):
+    """Defines the base multi-geometry class, holds
+    methods for dealing with multi-point data and allows
+    for the conversion of Multi-geo into other objects.
+    """
+    def __init__(self, name, points):
+        super().__init__(name)
+        self.__name = name
+        self.__points = points
+
     def make_points(self):  # Returns a list of point objects
         # Converts points list of coordinates to a list of Point objects, name increments with index +1
-        return [Point("Point " + str(self.__points.index(i) + 1), i[0], i[1]) for i in self.__points]
+        return [Point('Point ' + str(self.__points.index(i) + 1), i[0], i[1]) for i in self.__points]
 
-    def make_poly(self):
+    def make_poly(self):  # Returns a Polygon object
         return Polygon(self.__name, self.__points)
 
+    def invert(self):  # Returns inverted coordinates
+        return [[c * -1 for c in p] for p in self.__points]  # c = coordinate, p = point
 
-class Point:  # Points class to have methods get_x, get_y
+    def scale(self, factor):  # Returns coordinates scaled by a given factor
+        return [[c * factor for c in p] for p in self.__points]  # c = coordinate, p = point
 
+
+class Point(Geometry):
+    """Initialised by a name, then x and y
+    value as separate parameters.
+    """
     def __init__(self, name, x, y):
+        super().__init__(name)
         self.__name = name
         self.__x = x
         self.__y = y
         self.__count = 0
         self.__state = False
-
-    def get_name(self):
-        return self.__name
 
     def get_x(self):
         return self.__x
@@ -48,23 +70,23 @@ class Point:  # Points class to have methods get_x, get_y
     def get_y(self):
         return self.__y
 
-    def get_coords(self):  # This method is useful for when converting between geometry objects
+    def get_coords(self):  # Return a coordinate pair list
         return [self.__x, self.__y]
 
     def set_state(self, state):
-        valid_states = ["Boundary", "Inside", "Outside", "in_bb"]
-        if state in valid_states or [v.lower() for v in valid_states]:
-            self.__state = state.lower()
-        else:
-            print("Invalid point state given")
+        # This method is used for applying the point-polygon relationship after checking
+        self.__state = state.lower()
 
     def get_state(self):
         return self.__state
 
 
-class Line:  # Construct line given a name and 2 points yx lists, then constructs point objects
-
+class Line(Geometry):
+    """Initialised by a name, then
+    2 comma-separated x, y lists.
+    """
     def __init__(self, name, p1, p2):
+        super().__init__(name)
         self.__name = name
         self.__p1 = Point(str(name) + ": 1", p1[0], p1[1])
         self.__p2 = Point(str(name) + ": 2", p2[0], p2[1])
@@ -81,38 +103,39 @@ class Line:  # Construct line given a name and 2 points yx lists, then construct
             parallel_x = True
         else:
             parallel_x = False
-        if self.__p1.get_x() != math.inf:
-            if self.__p1.get_x() == self.__p2.get_x():
-                parallel_y = True
-            else:
-                parallel_y = False
+        if self.__p1.get_x() == self.__p2.get_x():
+            parallel_y = True
+        else:
+            parallel_y = False
         return [parallel_x, parallel_y]
 
-    def increment(self):
+    def increment(self):  # Used for counting the number of intersections
         self.__count += 1
 
     def get_count(self):
         return self.__count
 
 
-class Polygon:
-    # Initialize object with a list of point pairs as second argument
+class Polygon(MultiGeometry):
+    """Initialize object with name, then a list
+    of point coordinate pairs as second argument.
+    """
     def __init__(self, name, points):
+        super().__init__(name, points)
         self.__name = name
         self.__points = points
 
-    def invert(self):  # Can invert shapes
-        self.__points = [[c * -1 for c in p] for p in self.__points]  # c = coordinate, p = point
+    def invert(self):  # See MultiGeometry
+        return Polygon(self.__name, super().invert())
 
-    def get_coords(self):  # Return a list fo coordinate pair lists
+    def scale(self, factor):  # See MultiGeometry
+        return Polygon(self.__name, super().scale(factor))
+
+    def get_coords(self):  # Return a list of coordinate pair lists
         return self.__points
 
-    def make_points(self):  # Returns a list of point objects
-        # Converts points list of coordinates to a list of Point objects, name increments with index +1
-        return [Point("Point " + str(self.__points.index(i) + 1), i[0], i[1]) for i in self.__points]
-
     def get_lines(self):  # Return a list of Line objects
-        points = self.make_points()
+        points = super().make_points()
         lines = []
         pnt_a = points[0]
         for pnt_b in points[1:]:
@@ -121,33 +144,26 @@ class Polygon:
         lines.append(Line(pnt_a.get_name() + "-" + points[0].get_name(), pnt_a.get_coords(), points[0].get_coords()))
         return lines
 
-    def all_x(self):
+    def all_x(self):  # Returns a list of x values
         return [i[0] for i in self.get_coords()]
 
-    def all_y(self):
+    def all_y(self):  # Returns a list of y values
         return [i[1] for i in self.get_coords()]
 
-
-class Triangle(Polygon):
-    def __init__(self, name, p1, p2, p3):
-        super().__init__(name, [p1, p2, p3])
-
-    def get_area(self):  # Uses the shoelace formula in order to return area
-        points = self.make_points()
-        res = points[1].get_x() * points[2].get_y() - points[2].get_x() * points[1].get_y()
-        res = res - points[0].get_x() * points[2].get_y() + points[2].get_x() * points[0].get_y()
-        res = res + points[0].get_x() * points[1].get_y() - points[1].get_x() * points[0].get_y()
-        return abs(res) / 2
-
-
-class Square(Polygon):  # Obtains the 4 point locations from the bottom left point and side in attribute assignment
-    def __init__(self, name, bl_point, side):
-        bl_point = Point("Bottom left", bl_point[0], bl_point[1])
-        points = [bl_point, Point("Top left", bl_point.get_x(), bl_point.get_y() + side),
-                  Point("Top right", bl_point.get_x() + side, bl_point.get_y() + side),
-                  Point("Bottom right", bl_point.get_x(), bl_point.get_y() + side)]
-        super().__init__(name, points)  # Then initialises the points using the parent class
-        self.__side = side
-
     def get_area(self):
-        return self.__side ** 2
+        """Returns shape area based in the shoelace algorithm.
+        Supports closed and open shapes.
+        """
+        sum_x = 0.0
+        sum_y = 0.0
+        last_v = -1
+        if super().make_points()[0].get_coords() == super().make_points()[-1].get_coords():
+            last_v = -2  # if polygon is closed ignore last point as it is the same as first
+        for i in range(len(self.all_x()[:last_v])):  # Shoelace formula
+            sum_x += self.all_x()[i] * self.all_y()[i+1]
+            sum_y += self.all_x()[i+1] * self.all_y()[i]
+        sum_x += self.all_x()[last_v] * self.all_y()[0]
+        sum_y += self.all_x()[0] * self.all_y()[last_v]
+        return abs(sum_x - sum_y)/2
+
+
--- tests.py
+++ tests.py
@@ -1,10 +1,15 @@
+
+"""This file holds all functions for testing point-polygon geometry relationships."""
+
 import geometry_class as geo
 import math
 import matplotlib.pyplot as plt
 
 
-# Check if point is inside or on boundary of bounding box rectangle -
 def in_bb(points, poly):
+    """This function checks if point is inside
+    or on boundary of bounding box rectangle
+    """
     for p in points:
         if min(poly.all_x()) <= p.get_x() <= max(poly.all_x()) and min(poly.all_y()) <= p.get_y() <= max(poly.all_y()):
             p.set_state("in_bb")
@@ -12,54 +17,56 @@ def in_bb(points, poly):
 
 # Check if points lie on parallel line
 def on_line(points, poly):
-    # Loop through lines
-    for point in points:  # Loop through points that haven't been checked
+    """This function checks point-on-line
+    relationships using the cross-product method.
+
+    Method adapted from code from https://stackoverflow.com/questions/328107/
+    how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
+    """
+    for point in points:
         for line in poly.get_lines():
             x1, x2, x3 = line.get_p1().get_x(), line.get_p2().get_x(), point.get_x()
             y1, y2, y3 = line.get_p1().get_y(), line.get_p2().get_y(), point.get_y()
-            if (y3-y1) * (x2-x1) - (x3-x1) * (y2-y1) == 0:  # Cross product - points are aligned
+            if (y3-y1) * (x2-x1) - (x3-x1) * (y2-y1) == 0:  # if Cross product ==0: points are aligned
                 dot_prod = (x3 - x1) * (x2 - x1) + (y3 - y1) * (y2 - y1)
                 sqr_len_ab = (x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1)
                 if x1 <= x3 <= x2 or x1 >= x3 >= x2:  # Point is within line range
-                    if y3 == y1:  # if point is on line
+                    if y3 == y1 or 0 < dot_prod < sqr_len_ab:  # if point is on line
                         point.set_state("Boundary")
-                    elif 0 < dot_prod < sqr_len_ab:  # Method based on code from https://stackoverflow.com/questions/
-                        # 328107/how-can-you-determine-a-point-is-between-two-other-points-on-a-line-segment
-                        point.set_state("Boundary")
-                # elif line.is_parallel()[1]:  # Y parallel
                 if y1 <= y3 <= y2 or y1 >= y3 >= y2:  # Point is within line range
-                    if x3 == x1:  # if point is on line
+                    if x3 == x1 or 0 < dot_prod < sqr_len_ab:  # if point is on line
                         point.set_state("Boundary")
-                    elif 0 < dot_prod < sqr_len_ab:
-                        point.set_state("Boundary")
-
-
-def get_rays(points):
-    # Construct rays as a list of all points not classed as being on shape boundary
-    return [geo.Line("Ray " + str(points.index(p) + 1), p.get_coords(), [math.inf, p.get_y()]) for p in
-            [i for i in points if not i.get_state() == "Boundary"]]
 
 
 def special_cases(ray, poly, line, next_index, prev_index):
-    if ray.get_p1().get_y() == line.get_p1().get_y() and ray.get_p1().get_x() < line.get_p1().get_x():  # inline with vertex
+    """This function tests for special
+    cases in point-line relationships.
+    """
+    if ray.get_p1().get_y() == line.get_p1().get_y() and \
+            ray.get_p1().get_x() < line.get_p1().get_x():  # Inline and to left of vertex
         next_l = poly.get_lines()[next_index]
-        prev_l = poly.get_lines()[prev_index]  # Check  second to last vertex as last vertex == first
+        prev_l = poly.get_lines()[prev_index]
         if line.is_parallel()[0]:
+            # If parallel check that neighbour+1 nodes are above and below line, will not class an entry otherwise
             if next_l.get_p2().get_y() < ray.get_p1().get_y() < prev_l.get_p1().get_y() or \
                     next_l.get_p2().get_y() > ray.get_p1().get_y() > prev_l.get_p1().get_y():
                 ray.increment()
         elif prev_l.get_p1().get_y() < ray.get_p1().get_y() < next_l.get_p1().get_y() or \
                 prev_l.get_p1().get_y() > ray.get_p1().get_y() > next_l.get_p1().get_y():
-            # if not line.is_parallel()[0]:
             ray.increment()  # Neighbouring vertices of intersected polygon vertex are above and below ray
 
 
 def ray_caster(points, poly):
-    rays = get_rays(points)
+    """This function conducts the ray casting line
+    intersection algorithm then runs the special cases.
+    """
+    # Rays are lines constructed from points that haven't been classed as boundary
+    rays = [geo.Line("Ray " + str(points.index(p) + 1), p.get_coords(), [math.inf, p.get_y()]) for p in
+            [i for i in points if not i.get_state() == "Boundary"]]
     for ray in rays:  # [x1, y1], [x inf, y2]
         for line in poly.get_lines():  # [x3, y3], [x4, y4]
-            y1, y3, y4 = ray.get_p1().get_y(), line.get_p1().get_y(), line.get_p2().get_y()  # y coords
-            x1, x3, x4 = ray.get_p1().get_x(), line.get_p1().get_x(), line.get_p2().get_x()  # x coords
+            y1, y3, y4 = ray.get_p1().get_y(), line.get_p1().get_y(), line.get_p2().get_y()
+            x1, x3, x4 = ray.get_p1().get_x(), line.get_p1().get_x(), line.get_p2().get_x()
             if y3 < y1 < y4 or y3 > y1 > y4:  # point within line y range
                 try:
                     x = x3 + ((y1-y3) * (x4 - x3))/(y4 - y3)  # line intersection equation
@@ -69,15 +76,10 @@ def ray_caster(points, poly):
                     ray.increment()
         # All lines except first and last
         for i, line in enumerate(poly.get_lines()[1:-1]):
-            special_cases(ray, poly, line, i+2, i)  # Index is +1 due to omitting the 0th index from loop
+            special_cases(ray, poly, line, i+2, i)  # next/prev line index is +1 due to omitting the 0th index from loop
 
-        special_cases(ray, poly, poly.get_lines()[0], 1, -2)  # First line
+        # First line
+        special_cases(ray, poly, poly.get_lines()[0], 1, -2)  # prev_l is second to last vertex as last vertex == first
 
         if int(ray.get_count()) % 2 == 1:
             points[rays.index(ray)].set_state("Inside")
-
-    plt.fill(poly.all_x(), poly.all_y())
-    plt.plot([p.get_x() for p in points], [p.get_y() for p in points], 'r+')
-    for i, txt in enumerate(p.get_count() for p in rays):  # plot number of intersections on points
-        plt.annotate(txt, ([p.get_x() for p in points][i], [p.get_y() for p in points][i]))
-    plt.show()
